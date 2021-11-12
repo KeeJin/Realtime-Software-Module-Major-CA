3d5895778aae49b93cf77962389db25ed769c7ae
Cynexfdxf <71435483+Cynexfdxf@users.noreply.github.com>
2021-11-12 16:20:40 +0800

    modified build_app_tui.sh to work in QNX; created build_hardware.sh for hardware script

--- scripts/build_app_tui.sh
+++ scripts/build_app_tui.sh
@@ -1,16 +1,8 @@
 # Simple script to automate compilation and execution
-# echo "INFO: This script assumes that you have cloned the repository in your home directory."
+echo "INFO: This script assumes that you have cloned the repository in your home directory."
 echo "Compiling..."
-src_dir=$(dirname "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")
-
-# Check if a directory does not exist
-if [ ! -d "${src_dir}/../build" ] 
-then
-    echo "Creating build folder..." 
-    mkdir ${src_dir}/../build
-fi
-
-cc -o ${src_dir}/../build/app_tui ${src_dir}/../app/app_tui.c -lncurses -lm
+cd ../build
+cc -o app_tui ../app/app_tui.c -lncurses -lm
 echo "Compilation complete. Running executable... "
 echo "======================================================="
-${src_dir}/../build/app_tui
+./app_tui
