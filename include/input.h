1881b5001f718c8930e813dc937a479be3038ef6
KeeJin <yoplocheo@gmail.com>
2021-11-19 17:11:01 +0800

    minor edit

--- include/input.h
+++ include/input.h
@@ -9,14 +9,6 @@ FILE *fp;
 void *hardware_input_thread(void *arg); // Thread function for
                                         // interfacing with hardware peripherals
 
-// Function to read the potentiometer from the hardware
-void read_potentiometer(uint16_t *readpotentiometer1,
-                        uint16_t *readpotentiometer2);
-
-
-// Function to write to LED on the hardware
-void update_LED(unsigned int dio_switch);
-
 // Functions to convert the raw switch data into individual switch states
 int switch0_value(int switch_value);
 int switch1_value(int switch_value);
