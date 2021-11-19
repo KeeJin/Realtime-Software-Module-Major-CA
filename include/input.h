c12ba8815f684b5525670ddd87e69955d627aa8a
KeeJin <yoplocheo@gmail.com>
2021-11-19 15:56:46 +0800

    some comments

--- include/input.h
+++ include/input.h
@@ -1,16 +1,20 @@
-//headerfile for input.c
+// headerfile for input.c
 
 #ifndef INPUT_H
 #define INPUT_H
 
 #include "common.h"
 
-void *hardware_input_thread(void *arg);
-
+/* ----------------------------- Functions -------------------------------- */
+void *hardware_input_thread(void *arg); // Thread function for
+                                        // interfacing with hardware peripherals
+                                        
+// Functions for checking the status of individual digital switches
 int switch0_value(int switch_value);
 int switch1_value(int switch_value);
 int switch2_value(int switch_value);
 int switch3_value(int switch_value);
+/* ------------------------------------------------------------------------ */
 
 FILE *fp;
 
